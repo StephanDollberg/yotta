e1c9d42a941fcf19fe4485a8596df855e5b90630
Stephan Dollberg <stephan.dollberg@gmail.com>
2016-12-03 10:28:15 +0000

    add upgrade test

---
+++ src/test/run_upgrade_test.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+$(which yotta) "::" 10000 /tmp/yotta_update_test.pid &
+pid=$!
+sleep 1
+kill -s SIGUSR1 $pid
+kill -s SIGQUIT $(cat /tmp/yotta_update_test.pid.old)
+curl http://localhost:10000
+kill -s SIGQUIT $(cat /tmp/yotta_update_test.pid)
